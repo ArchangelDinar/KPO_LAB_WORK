d1d71ab9e59b23c29da15077ea99144b6f8e8b13
ArchangelDinar <34435150+ArchangelDinar@users.noreply.github.com>
2017-12-11 13:26:03 +0300

    version 1.3
    IDateFileRow interface implemented

---
+++ KPO_4311_ADM.Lib/Source/Container/IDataFileRow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPO_4311_ADM.Lib.Source.Container
+{
+    public interface IDataFileRow
+    {
+        int[] format { get; set; }
+        string fromArrayToString(string[] array);
+        string[] fromStringToArray(string str);
+    }
+    public class DataFileRow : IDataFileRow
+    {
+        public int[] format
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string fromArrayToString(string[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] fromStringToArray(string str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
